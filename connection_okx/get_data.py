934bf49e5e7cfbb5d72de25a17166f74ac20d9ad
user <sheshukov458@gmail.com>
2025-05-31 12:05:04 +0300

    изменил название функции date_two_years_ago на date_eight_month_ago, так как данных можно получить только за восемь предыдущих месяцев

--- connection_okx/get_data.py
+++ connection_okx/get_data.py
@@ -14,12 +14,33 @@ def get_local_tz() -> int:
     return int(offset_hours)
 
 
-def date_two_years_ago() -> datetime:
+def date_eight_month_ago() -> int:
+    hours_back = 1440 * 4
     now = datetime.now()
-    two_years_ago = now - timedelta(days=365 * 2)
-    return two_years_ago.replace(microsecond=0)
+    eight_month_ago = now - timedelta(hours=hours_back - 4)
+    return int(eight_month_ago.replace(microsecond=0).timestamp() * 1000)
 
 
+def now_date() -> int:
+    now = datetime.now()
+    return int(now.replace(microsecond=0).timestamp() * 1000)
+
+
+start_date = date_eight_month_ago()
+end_date = now_date()
+print('старт', start_date, 'конец', end_date)
+
+print(4 * 3600 * 1000 * 300)
+
+
+while start_date < end_date:
+    start_date += 4 * 3600 * 1000 * 300
+
+    if start_date > end_date:
+        print('конец', end_date)
+    print('старт', start_date)
+
+# print(int(date_two_years_ago().timestamp() * 1000), date_two_years_ago())
 # def get_history_candles(inst_id='BTC-USDT', bar='4H', limit=100):
 #     endpoint = '/api/v5/market/history-candles'
 #     params = {
@@ -42,24 +63,30 @@ def date_two_years_ago() -> datetime:
 #
 # get_history_candles()
 
-
+# start_date = int(date_two_years_ago().timestamp() * 1000
+# print(str(int(start_date.timestamp() * 1000)))
+# end_date =
+#
+#
+#
 # endpoint = '/api/v5/market/candles'
 # params = {
 #     'instId': 'BTC-USDT',
 #     'bar': '4H',  # 1-day candles
-#     'limit': '100'  # Last 100 days
+#     'limit': '300',  # Last 100 days
+#     'after': '1749560050000'
 # }
 #
 # url = 'https://www.okx.com' + endpoint
 # response = requests.get(url, params=params)
 # print(response.json())
-#
+# #
 # df = pd.DataFrame(response.json()['data'])
 #
 # columns = ['ts', 'open', 'high', 'lowest', 'close', 'volume', 'volCcy', 'volCcyQuote', 'confirm']
 #
 # df.columns = columns
+# print(df)
 #
 # df['ts'] = pd.to_datetime(df['ts'], unit='ms') + pd.Timedelta(hours=get_local_tz())
 # print(df)
-
