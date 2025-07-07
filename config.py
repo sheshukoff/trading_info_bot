0aa851c942bf6537244a9f690e4b324830f5af07
user <sheshukov458@gmail.com>
2025-07-07 12:22:17 +0300

    удалил

--- config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    RABBITMQ_URL = "amqp://guest:guest@localhost/"
-    BOT_TOKEN = '7593174213:AAHlEksHs8rN93gL9eEJ-tVuecKCkUHhJR4'
--- test1_MQ/keyboars.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Каталог')],
-    [KeyboardButton(text='RSI 14'), KeyboardButton(text='Цены по BTC')]
-],
-    resize_keyboard=True,
-    input_field_placeholder='Выберите пункт меню'
-)
