341ee65993513fb1c647625d162c25fd6531ccf4
user <sheshukov458@gmail.com>
2025-07-10 19:18:43 +0300

    начало работать после того как я привел import порядок

--- telegram/application/handlers.py
+++ telegram/application/handlers.py
@@ -3,7 +3,8 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from rmq.rabbit import send_to_queue
 from aiogram import types
-import keyboards as kb
+
+import telegram.application.keyboards as kb
 
 router = Router()
 
@@ -47,35 +48,3 @@ async def handle_number(message: types.Message):
 
     except ValueError:
         await message.answer("❌ Пожалуйста, отправьте целое число!")
-
-
-# @router.message(Command('help'))
-# async def command_help_handler(message: Message):
-#     await message.answer('Это команда /help')
-#
-#
-# @router.message(F.text == 'Как дела?')
-# async def how_are_you(message: Message):
-#     await message.answer('ОК!')
-#
-#
-# @router.message(F.photo)
-# async def get_photo(message: Message):
-#     await message.answer(f'ID фото {message.photo[-1].file_id}')
-#
-#
-# @router.message(Command('get_photo'))
-# async def get_photo(message: Message):
-#     await message.answer_photo(
-#         photo='https://petsitters.by/uploads/cache/750x500/02/48/86/image1711981679_EJI16nZVbKErYZpN.jpg',
-#         caption='На держи мопса'
-#     )
-#
-#
-# @router.callback_query(F.data == 'catalog')
-# async def catalog(callback: CallbackQuery):
-#     await callback.answer('Вы выбрали каталог')
-#     await callback.message.edit_text('Привет!', reply_markup=kb.settings)
-#
-#
-# # def send_rsi_4h
