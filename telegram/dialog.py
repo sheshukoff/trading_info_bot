0b223c3b4d2cf797ce070a5e67c43507066a9e2e
user <sheshukov458@gmail.com>
2025-09-17 11:17:15 +0300

    сам диалог

---
+++ telegram/dialog.py
@@ -0,0 +1,44 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart
+
+
+from aiogram_dialog import Dialog, setup_dialogs
+
+from dotenv import dotenv_values
+from telegram.handlers import router, start
+from telegram.windows_for_dialogs import (
+    window_start, window_disclaimer, window_strategy, window_coins,window_alarm_times, window_confirmation
+)
+
+dialog = Dialog(
+    window_start, window_disclaimer, window_strategy, window_coins, window_alarm_times, window_confirmation
+)
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    config = dotenv_values("../.env")
+    BOT_TOKEN = config.get("BOT_TOKEN")
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(dialog)
+    dp.include_router(router)
+    dp.message.register(start, CommandStart())
+
+    setup_dialogs(dp)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('exit')
+
+# Планы на сегодня:
+# Закончить добавление стратегии
+# Затем запусктить эти стратегии в работу
+# По возможности сделать вывод информации (что делать покупать или продавать)
