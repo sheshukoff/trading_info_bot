f3cd80b242e9269500c9de9659cfbde93899da10
user <sheshukov458@gmail.com>
2025-09-17 11:17:47 +0300

    handlers для управления диалогами и кнопками

---
+++ telegram/handlers.py
@@ -0,0 +1,69 @@
+from aiogram_dialog import DialogManager, ChatEvent, StartMode
+
+from aiogram_dialog.widgets.kbd import ManagedCheckbox, Select
+from aiogram import Router, F
+
+from aiogram.types import CallbackQuery, Message
+from telegram.app.keyboards import main_menu
+from telegram.states import MainSG
+
+router = Router()
+
+
+async def start(message: Message, dialog_manager: DialogManager):
+    await dialog_manager.start(MainSG.start, mode=StartMode.RESET_STACK)
+
+
+async def on_start_menu(callback, button, manager: DialogManager):
+    await manager.done()  # закрываем диалог
+
+    # безопасно получаем chat_id
+    if manager.event.message:
+        chat_id = manager.event.message.chat.id
+    else:
+        chat_id = manager.event.from_user.id
+
+    await manager.event.bot.send_message(
+        chat_id=chat_id,
+        text="📋 Главное меню:",
+        reply_markup=main_menu
+    )
+
+
+def make_on_selected(key: str, next_state):
+    """Фабрика универсальных обработчиков выбора"""
+    async def handler(c: CallbackQuery, s: Select, manager: DialogManager, selected: str):
+        manager.dialog_data[key] = selected
+        await manager.switch_to(next_state)
+    return handler
+
+
+async def on_add_strategy(c, b, manager: DialogManager):
+    await manager.switch_to(MainSG.strategies)
+
+
+async def on_agree_changed(event: ChatEvent, checkbox: ManagedCheckbox, manager: DialogManager):
+    manager.dialog_data["agree"] = checkbox.is_checked()
+    print("Agree status:", manager.dialog_data["agree"])
+
+
+def selected_data(key: str, alias: str):
+    """
+    Универсальная фабрика getter'ов для dialog_data.
+    key   - ключ в dialog_data
+    alias - как будет называться в шаблоне
+    """
+    async def getter(dialog_manager: DialogManager, **kwargs):
+        return {alias or key: dialog_manager.dialog_data.get(key, [])}
+    return getter
+
+
+@router.message(F.text == 'Информация о боте')
+async def info_about_bot(message: Message):
+    await message.reply('Дополнительная информация о боте')
+
+
+@router.message(F.text == 'Добавить стратегию')
+async def choose_strategy(message: Message, dialog_manager: DialogManager):
+    await dialog_manager.start(MainSG.strategies, mode=StartMode.RESET_STACK)
+
