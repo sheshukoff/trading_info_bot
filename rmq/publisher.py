d06a245a5f96e51efe8b84cda8a3a8235930ec4f
user <sheshukov458@gmail.com>
2025-09-22 20:16:54 +0300

    пока работает через окно rabbit_mq публикует сообщения что делать покупать или продавать

---
+++ rmq/publisher.py
@@ -0,0 +1,46 @@
+import asyncio
+import aiorabbit
+import datetime
+from dotenv import dotenv_values
+
+config = dotenv_values("../.env")
+RABBITMQ_URL = config.get("RABBITMQ_URL")
+
+
+async def periodic_publisher(message):
+    try:
+        async with aiorabbit.connect(RABBITMQ_URL) as client:
+            await client.confirm_select()
+            await client.queue_declare('periodic_queue')
+
+            success = await client.publish(
+                routing_key='periodic_queue',
+                message_body=message,
+                content_type='text/plain',
+                exchange='',  # необязательно
+                app_id='periodic_publisher'  # необязательно
+            )
+
+            if success:
+                print(f'Отправлено: {message}')
+            else:
+                print('Ошибка отправки сообщения!!!')
+
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+# core <- tasks
+# core -> periodic_queue (5sec)
+
+# message_count = 0
+#
+# while True:
+#     message_count += 1
+#
+#     message_body = (
+#         f'Сообщение #{message_count}'
+#         f'отправлено в {datetime.datetime.now()}'
+#     )
+
+# await asyncio.sleep(5)
