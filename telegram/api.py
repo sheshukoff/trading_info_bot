c47b936ca25fb0484ff0ee5683e9d74afe55fc26
user <sheshukov458@gmail.com>
2025-10-18 22:28:49 +0300

    Есть две функции add_user и delete_user

---
+++ telegram/api.py
@@ -0,0 +1,29 @@
+import requests
+import asyncio
+
+URL = 'http://127.0.0.1:8000'
+HEADERS = {'Content-Type': 'application/json'}
+
+
+async def add_user(telegram_id: int, telegram_name: str):
+    body = {
+        "telegram_id": telegram_id,
+        "telegram_name": telegram_name
+    }
+
+    end_point = f'{URL}/users'
+    response = requests.post(end_point, json=body, headers=HEADERS)
+    await asyncio.sleep(0.1)
+    print(response.json())
+    return response.json()['id']
+
+
+async def delete_user(telegram_id: int):
+    body = {
+        "telegram_id": telegram_id
+    }
+
+    end_point = f'{URL}/users'
+    response = requests.delete(end_point, json=body, headers=HEADERS)
+    await asyncio.sleep(0.1)
+    return response.json()['id']
