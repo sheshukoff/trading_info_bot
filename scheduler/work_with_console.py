f936a8633eda2e1091243c4d75f6f78e6d7aedab
user <sheshukov458@gmail.com>
2025-11-11 18:07:55 +0300

    изменил функцию вынрузки данных

--- scheduler/work_with_console.py
+++ scheduler/work_with_console.py
@@ -1,5 +1,5 @@
 import asyncio
-from connection_okx.get_data import get_history_data_okx
+from connection_okx.get_data import get_data_okx
 from strategies.strategies import rsi_strategy, ema_strategy
 from scheduler.scheduler import Scheduler
 
@@ -7,8 +7,8 @@ SCHEDULERS = {}
 RUN_TASKS = {}
 
 strategies = {
-    1: ("RSI стратегия", get_history_data_okx, rsi_strategy),
-    2: ("EMA стратегия", get_history_data_okx, ema_strategy),
+    1: ("RSI стратегия", get_data_okx, rsi_strategy),
+    2: ("EMA стратегия", get_data_okx, ema_strategy),
 }
 
 
