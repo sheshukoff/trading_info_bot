c4043d8ef5cc70434ea9c05317c15e6c6b03355a
user <sheshukov458@gmail.com>
2026-01-01 17:53:56 +0300

    Написал новый функционал теперь пользователь не может выбрать более 2 стратегий

--- telegram/data_for_dialog.py
+++ telegram/data_for_dialog.py
@@ -1,4 +1,5 @@
 from connection_oracle.queries_to_oracle import get_coins, get_alarm_times
+from connection_oracle.get_queries import get_quantity_strategy_user
 
 
 async def get_strategies_data(dialog_manager, **kwargs):
@@ -16,3 +17,13 @@ async def get_coins_data(dialog_manager, **kwargs):
 async def get_alarm_times_data(dialog_manager, **kwargs):
     alarm_times = await get_alarm_times()
     return {'alarm_times': alarm_times}
+
+
+async def get_max_strategy_user(dialog_manager, **kwargs):
+    if dialog_manager.event.message:
+        chat_id = dialog_manager.event.message.chat.id
+    else:
+        chat_id = dialog_manager.event.from_user.id
+
+    max_strategy = await get_quantity_strategy_user(chat_id)
+    return {'limit': max_strategy}
